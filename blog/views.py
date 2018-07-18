e367e4b53393304ef1de5b5d9026d7833d348dc1
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-18 19:11:07 +0300

    blog detail

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,27 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import PostModel
 
-@login_required(login_url='/login/')
+# Create your views here.
+
+# @login_required(login_url='/login/')
 def post_model_list_view(request):
     qs = PostModel.objects.all()
 
-    if request.user.is_authenticated():
-        print('logged in')
-    else:
-        print('not logged in')
-
-    if request.user.is_authenticated():
-        template = 'blog/list-view.html'
-    else:
-        template = 'blog/list-view-public.html'
-
+    template = 'blog/list-view.html'
     context = { 'object_list': qs }
+
+    return render(request, template, context)
+
+
+def post_model_detail_view(request, id):
+    # try:
+    #     obj = PostModel.objects.get(id)
+    # except:
+    #     raise Http404
+
+    obj = get_object_or_404(PostModel, id=id)
+    context = { "object": obj }
+    template = "blog/detail-view.html"
     return render(request, template, context)
