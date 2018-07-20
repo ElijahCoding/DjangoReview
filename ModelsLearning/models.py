0c2cc31ccd6c1331aca801c0e32c895dd13d2ce2
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-20 16:13:20 +0300

    models choices

--- ModelsLearning/models.py
+++ ModelsLearning/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.db.models import Model
 
+PUBLISH_CHOICES = (
+    ('draft', 'Draft'),
+    ('publish', 'Publish'),
+    ('private', 'Private')
+)
+
 # Create your models here.
 class PostModel(Model):
     id = models.AutoField(primary_key=True)
     active = models.BooleanField(default=True)
     title = models.CharField(max_length=240)
     content = models.TextField(null=True, blank=True)
+    publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
 
     class Meta:
         verbose_name = 'Post'
