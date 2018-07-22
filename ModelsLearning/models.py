bcd3a577e1a6dd647b21cbf8ff5ee1dfe285b47c
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-22 15:17:43 +0300

    overriding the save method

--- ModelsLearning/models.py
+++ ModelsLearning/models.py
@@ -3,6 +3,8 @@ from django.db.models import Model
 from django.utils.encoding import smart_text
 from django.utils import timezone
 from .validators import validate_author_email
+from django.utils.text import slugify
+
 
 PUBLISH_CHOICES = (
     ('draft', 'Draft'),
@@ -14,12 +16,18 @@ PUBLISH_CHOICES = (
 class PostModel(Model):
     id = models.AutoField(primary_key=True)
     active = models.BooleanField(default=True)
-    title = models.CharField(max_length=240, unique=True)
+    title = models.CharField(max_length=240, verbose_name='Post title', unique=True)
+    slug = models.SlugField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
     view_count = models.IntegerField(default=0)
     publish_data = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
     author_email = models.EmailField(max_length=240, validators=[validate_author_email], null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.title:
+            self.slug = slugify(self.title)
+        super(PostModel, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Post'
